204dbf62aa72dd07ef238f6f0c01e8974ae574d1
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2020-01-29 18:26:12 +0700

    refactor accepting streams from adonis request
    the method before was quite messy, since my understanding about request streaming
    was quite shallow back then. the request object was thrown every where,
    using unnecessary callback, and my implementation to create stream
    from google library was quite ambiguous.
    
    I believe this was the proper flow.

--- src/controllers/RamenFileController.js
+++ src/controllers/RamenFileController.js
@@ -7,22 +7,55 @@ class RamenFileController {
 
     async uploadFile({request, response}) {
         response.implicitEnd = false
-        
-        await FileProvider.uploadFile(request, (data, err) => {
-            if (data) {
-                return response.status(200).send({
-                    data: {
-                        fileUrl: data
-                    },
-                    meta: {
-                        message: 'upload successfull'
-                    }
-                })
+        let extnames = []
+        let fileName = null
+        let fileStream = null
+
+        if (request.input('extnames')) {
+            extnames = request.input('extnames').split(',')
+        }
+
+        request.multipart.file(
+            'file',
+            {
+                size: '10mb',
+                extnames: extnames
+            },
+            async (file) => {
+                await file.runValidations()
+                const error = file.error()
+                if (error.message) {
+                    response.status(422).send({
+                        data: null,
+                        meta: {
+                            message: error.message
+                        }
+                    })
+                }
+
+                fileStream = FileProvider.createFileStream(file.headers['content-type'])
+                fileName = FileProvider.createFilename(file.type, file.clientName)
+                file.stream.pipe(fileStream)
             }
-            return response.status(500).send({
+        ).process()
+
+        fileStream.on('error', (err) => {
+            response.status(500).send({
                 data: null,
                 meta: {
                     message: err
+                }
+            })
+        })
+
+        fileStream.on('finish', () => {
+            fileName = FileProvider.getPublicUrl(fileName)
+            return response.status(200).send({
+                data: {
+                    fileUrl: fileName
+                },
+                meta: {
+                    message: 'upload successfull'
                 }
             })
         })
--- src/resolver/GoogleFileResolver.js
+++ src/resolver/GoogleFileResolver.js
@@ -14,56 +14,23 @@ class GoogleFileResolver {
         this.bucket = storage.bucket(this.bucketName)
     }
 
-    async uploadFile(request, callback) {
-        let fileName = null
-        let fileStream = null
-
-        await request.multipart.file(
-            'file', 
-            {
-                size: '10mb',
-                extnames: ['png', 'jpg', 'jpeg', 'pdf', 'doc', 'docx', 'xls', 'xlsx']
-            }, 
-            async (file) => {
-                await file.runValidations()
-                const error = file.error()
-                if (error.message) {
-                    callback(null, error.message)
-                    return
-                }
-
-                const result = this.createFileStream(file)
-                fileStream = result.stream
-                fileName = result.name
-            }
-        )
-        .process()
-
-        fileStream.on('error', function(err) {
-            callback(null, err)
-        })
-        fileStream.on('finish', () => {
-            fileName = this.getPublicUrl(fileName)
-            callback(fileName, null)
-        })
-    }
-
-    createFileStream(file) {
-        let objectName = file.type + '/' + Date.now() + '-' + file.clientName
+    static createFileStream(contentType) {
         let fileData = this.bucket.file(objectName)
         let fileStream = fileData.createWriteStream({
             metadata: {
-                contentType: file.headers['content-type']
+                contentType: contentType
             },
             resumable: false,
             public: true
         })
-
-        file.stream.pipe(fileStream)
-        return {name: objectName, stream: fileStream}
+        return fileStream
     }
 
-    getPublicUrl(filename) {
+    static createFileName(type, name) {
+        return type + '/' + Date.now() + '-' + name
+    }
+
+    static getPublicUrl(filename) {
         return 'https://storage.googleapis.com/' + this.bucketName + '/' + filename
     }
 }
